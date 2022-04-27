5c1da702173342493995c2b255b065c79abd67bf
davi-s-silva <69742480+Davi-S-Silva@users.noreply.github.com>
2022-04-27 18:23:26 -0300

    add conteudo

--- resources/views/includes/content.blade.php
+++ resources/views/includes/content.blade.php
@@ -1,24 +1,28 @@
 @extends('main')
 @section('home')
     <div id="servicos">
-        <h1>servicos</h1>
+        {{-- <h1>servicos</h1> --}}
     </div>
     <div id="planos">
+        <figure>
+            <img src="{{ asset('images/banner_1.jpg') }}" alt="">
+        </figure>
         @for ($i = 0; $i < 6; $i++)
-            <section class="planos col-3">
+            <section class="planos col-12 col-sm-3 col-md-3 col-lg-3">
                 <header>
                     <p> 60 </p>
                     <p> mega </p>
                 </header>
                 <article>
                     <p>
-
-                        por apenas
-                        R$ 55,00/mes
-                        instalacao 50,00 *
-                        Roteador
-                        100% fibra optica
+                        Por apenas
+                        R$55,00/mes
                     </p>
+                    <ul>
+                        <li>Instalacao 50,00 *</li>
+                        <li>Roteador</li>
+                        <li>100% fibra optica</li>
+                    </ul>
 
                     <a href="">Assinar</a>
 
